e53a41b21445ed58981fb57a1c9ba58f980f1e47
Front_end_performer <sodolinskyiart@gmail.com>
2018-11-06 15:45:03 +0100

    Revert "valTest"
    This reverts commit 700cab3c53d8b0e114c14f7e336b90d83cb772ea.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-// import SignIn from './Screens/SignIn';
-// import LogIn from './Screens/controlledInput/index'
-// import Validation from './Screens/Disabling_Submit_Button/index'
-// import Form from './Screens/Form/index'
-// import Payment from './Screens/PaymentStage/index'
-// import PayStage from './Screens/PaymentCompStage/index'
-import SelectPayment from './SelectPayment/index'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <SignIn /> */}
-          {/* <LogIn /> */}
-          {/* <Validation /> */}
-          {/* <Form /> */}
-          {/* <Payment /> */}
-          {/* <PayStage /> */}
-          <SelectPayment />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
